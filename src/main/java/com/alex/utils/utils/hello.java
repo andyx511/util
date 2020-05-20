d3c0b5add61ab05541ad70a400e8b2838dc95255
1812425414 <1812425414@qq.com>
2020-05-20 09:45:12 +0800

    添加测试

---
+++ src/main/java/com/alex/utils/utils/hello.java
@@ -0,0 +1,14 @@
+package com.alex.utils.utils;
+
+/**
+ * @Classname hello
+ * @Description TODO
+ * @Date 2020/5/20 9:44
+ * @Author ALexNi
+ */
+public class hello
+{
+    public static void main(String[] args) {
+
+    }
+}
